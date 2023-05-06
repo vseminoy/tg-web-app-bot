9dccb9d112a4ad30e408ec9eb56cb2f047dcc642
semina.v@itbizon.com <0bmNrYu@VWAqD~>
2023-05-06 05:19:09 +0300

    init

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data',async (req,res) => {
-    const {queryId,products, totalPrice} = request.body;
+    const {queryId,products, totalPrice} = req.body;
     try{
         await bot.answerWebAppQuery(queryId,{
             type: 'article',
